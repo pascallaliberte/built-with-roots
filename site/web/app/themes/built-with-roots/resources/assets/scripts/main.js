3d3d1f865c3d6c548bdc0659eeebd5daa83a2b58
Pascal Laliberté <pascal@pascallaliberte.me>
2017-05-08 23:16:37 -0400

    Install latest Sage 9, configure theme name, settings

---
+++ site/web/app/themes/built-with-roots/resources/assets/scripts/main.js
@@ -0,0 +1,25 @@
+/** import external dependencies */
+import 'jquery';
+import 'bootstrap';
+
+/** import local dependencies */
+import Router from './util/Router';
+import common from './routes/common';
+import home from './routes/home';
+import aboutUs from './routes/about';
+
+/**
+ * Populate Router instance with DOM routes
+ * @type {Router} routes - An instance of our router
+ */
+const routes = new Router({
+  /** All pages */
+  common,
+  /** Home page */
+  home,
+  /** About Us page, note the change from about-us to aboutUs. */
+  aboutUs,
+});
+
+/** Load Events */
+jQuery(document).ready(() => routes.loadEvents());
