2991e21955e11524ae2b8daa50a9b694d2d55447
Pascal Laliberté <pascal@pascallaliberte.me>
2017-05-14 21:54:49 -0400

    Sites: fetch sites via API, display images and clean url as label

--- site/web/app/themes/built-with-roots/resources/assets/scripts/vue/site.vue
+++ site/web/app/themes/built-with-roots/resources/assets/scripts/vue/site.vue
@@ -1,5 +1,27 @@
 <template>
+  <div class="site">
+    <a v-if="site.acf.site_url" :href="site.acf.site_url">
+      <img v-if="site.acf.site_screenshot_desktop" class="site-desktop-image" :src="site.acf.site_screenshot_desktop.url">
+      <span class="site-label">
+        {{ site_label }}
+      </span>
+    </a>
+  </div>
 </template>
 
 <script>
+export default {
+  props: {
+    site: {
+      type: Object
+    }
+  },
+  computed: {
+    site_label() {
+      const regexp = /^https?:\/\/(.*)/
+      let matches = this.site.acf.site_url.match(regexp)
+      return matches[1]
+    }
+  }
+}
 </script>
--- site/web/app/themes/built-with-roots/resources/assets/scripts/vue/sites.vue
+++ site/web/app/themes/built-with-roots/resources/assets/scripts/vue/sites.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="sites">
-
+    <div v-for="site in sites">
+      <site :site="site"></site>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import Site from './site.vue'
+
+Vue.use(VueResource)
+
+const baseURL = "/wp-json/wp/v2/";
+
 export default {
+  components: {
+    Site
+  },
   data() {
     return {
-
+      perPage: 20,
+      pageNum: 1,
+      sites: [],
+      totalResults: null,
+      numPages: null,
     }
   },
   created() {
-
+    this.getSites();
+  },
+  methods: {
+    getSites() {
+      this.$http.get(this.siteListUrl).then(function(res) {
+        this.sites = res.body
+        this.totalResults = res.headers.get('X-WP-Total')
+        this.numPages = res.headers.get('X-WP-TotalPages')
+      }, function(res) {
+        console.log('error', res);
+      });
+    }
+  },
+  computed: {
+    siteListUrl() {
+      let url = baseURL + 'site'
+      url += "?per_page=" + this.perPage
+      url += "&page=" + this.pageNum
+      return url
+    }
   }
 }
 </script>
