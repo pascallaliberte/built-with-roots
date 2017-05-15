dcf809bd10192c538c78e8cb53aa42a269048fef
Pascal Laliberté <pascal@pascallaliberte.me>
2017-05-14 22:17:02 -0400

    Sites: calculate theme, position of url label, random size of image - Base these on the id of the post

--- site/web/app/themes/built-with-roots/resources/assets/scripts/vue/site.vue
+++ site/web/app/themes/built-with-roots/resources/assets/scripts/vue/site.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="site">
-    <a v-if="site.acf.site_url" :href="site.acf.site_url">
+    <a v-if="site.acf.site_url" :href="site.acf.site_url" :class="imageClass">
       <img v-if="site.acf.site_screenshot_desktop" class="img-fluid site-desktop-image" :src="site.acf.site_screenshot_desktop.url">
-      <span class="site-label">
-        {{ site_label }}
+      <span class="site-label" :class="labelClass">
+        {{ siteLabel }}
       </span>
     </a>
   </div>
@@ -17,10 +17,49 @@ export default {
     }
   },
   computed: {
-    site_label() {
+    siteLabel() {
       const regexp = /^https?:\/\/(.*)/
       let matches = this.site.acf.site_url.match(regexp)
       return matches[1]
+    },
+    labelClass() {
+      let c = ""
+      c += " " + this.labelClassTheme
+      c += " " + this.labelClassPosition
+      return c
+    },
+    labelClassTheme() {
+      let c = "site-label-theme-"
+      if ((this.site.id % 2 == 0 || this.site.id % 7 == 0 || this.site.id % 11 == 0) && this.site.id % 6 != 0) {
+        c += "light"
+      } else {
+        c += "dark"
+      }
+      return c
+    },
+    labelClassPosition() {
+      let c = "site-label-pos-"
+      if (this.site.id % 3 == 0) {
+        c += "left"
+      } else if (this.site.id % 3 == 1) {
+        c += "center"
+      } else {
+        c += "right"
+      }
+      return c
+    },
+    imageClass() {
+      let c = "site-image-size-"
+      if (this.site.id % 4 == 0) {
+        c += "sm"
+      } else if (this.site.id % 4 == 1) {
+        c += "lg"
+      } else if (this.site.id % 4 == 2) {
+        c += "xl"
+      } else {
+        c += "md"
+      }
+      return c
     }
   }
 }
